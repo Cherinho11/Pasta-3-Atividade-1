a279151330a90cff58d9af0df2e963d1a998125d
Cherinho11 <marcellagrau6@gmail.com>
2026-06-20 14:26:28 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,9 @@
+let notaFinal = prompt("Informe sua nota final:");
+
+if (notaFinal >= 7) {
+    alert("Aprovado");
+} else if (notaFinal >= 5 && notaFinal < 7) {
+    alert("Recuperação");
+} else {
+    alert("Reprovado");
+}
